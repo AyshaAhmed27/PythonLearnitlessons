564431e5a86c9e71ddd84751da18d89085865ec6
AyshaAhmed27 <97908217+AyshaAhmed27@users.noreply.github.com>
2024-08-07 13:17:40 +0530

    Initial Commit

---
+++ 2.data types and variables/basic pgm for data types.py
@@ -0,0 +1,9 @@
+#  Basic pgm for data types
+name= 'aysha'
+age= 34
+weight=65.5
+is_student= False
+print('Name :' , name)
+print('age:' , age)
+print('Weight:', weight)
+print('Are you a student:', is_student)
