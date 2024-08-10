6f06882a309a267a9ab4f7b093304b02d0e3e0c2
AyshaAhmed27 <97908217+AyshaAhmed27@users.noreply.github.com>
2024-08-10 21:06:28 +0530

    Initial Commit

---
+++ 2.data types and variables/shoppin_calc.py
@@ -0,0 +1,12 @@
+wallet=500
+onion=100
+tomato=200
+potato=50
+total_veggies= onion + tomato + potato
+price_left= wallet - total_veggies
+total_items=3
+average_of_veggies= total_veggies / total_items
+average_of_veggies=round(2)
+print("Total veggies in Rs:",total_veggies)
+print("Price left:",price_left)
+print("Average of total items:",average_of_veggies)
